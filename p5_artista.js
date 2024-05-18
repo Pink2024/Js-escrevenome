c4c982b8b9523b79bf9804cc908ec12201667baa
Pink2024 <elinha.martos@gmail.com>
2024-05-18 10:23:54 -0300

    Create p5_artista.js

---
+++ p5_artista.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(400, 400);
+   background(220);
+  
+}
+
+function draw() {.
+  fill("pink");
+  
+   if (mouseIsPressed)
+       {
+       rect(mouseX, mouseY, 20, 35);
+       
+       }
+     
+  
+ 
+}
